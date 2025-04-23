45c1c935027c90b660de9731caf9ae4e4acbf15e
Chao-Ming Wang <37169494+101100545@users.noreply.github.com>
2025-04-23 14:13:45 +1000

    Added calendar event input and html to pdf file function Functions work alone, but problems with agent to tool calling documentWriter agent added

--- assistant_agent.py
+++ assistant_agent.py
@@ -1,8 +1,8 @@
 from autogen import AssistantAgent, UserProxyAgent, ConversableAgent, register_function, LLMConfig, GroupChat
 import autogen
 from datetime import datetime
-from tools import get_calendar
-import pytz
+from xhtml2pdf import pisa
+from tools import get_calendar, create_ics_event, document_creator
 
 
 ollama_config = {
@@ -16,15 +16,36 @@ ollama_config = {
     ],
 }
 
+llm_config = {
+    "model": "mistral",
+    "api_type": "ollama",
+    "temperature": 0.5,
+}
+
 now = datetime.now()
 format_now = now.strftime("%A, %B %d, %Y at %I:%M %p")
 
-message = f"""You are a helpful AI assistant who will look at the user's calendar and suggest when they should plan a trip.
-You MUST use the 'get_calendar' function to check availability but do not mention this to the end user. The current date and time is {format_now}. 
-When calling 'get_calendar', if the calendar shows no availability in the next 3 months of the current date and time, suggest that they come back later. 
-Make a list of all the available times in a neat, readable format. Do not make up time and dates that have not been confirmed with the calendar.
-"""
+message = f"""
+You are a helpful AI assistant specializing in managing the user's calendar. Your primary role is to assist the user with scheduling, planning, and organizing their time effectively by interacting with their calendar.
 
+Your responsibilities include:
+1.  Checking calendar availability for specific dates, times, or periods based on user requests you must use this function 'get_calendar' to perform this task.
+2.  Suggesting suitable available times for activities like planning trips, meetings, or appointments, based on the calendar data you must use this function 'get_calendar' to perform this task.
+3.  Adding new events, appointments, or tasks to the calendar when the user provides clear instructions you must first use this function 'get_calendar' then use this function 'create_ics_event' to perform this task.
+
+When checking availability for planning purposes (such as suggesting times for a trip or appointment), if the calendar tool indicates no available slots within the next 3 months from the current date and time ({format_now}), please inform the user that the calendar appears full for that period and they should check back later.
+If the planned date and time to add a new event has coincides with another event been, please inform the user that it appears theres already an event planned for that time, and to kindly schedule a different time.
+If there are no time clashes, proceed to adding the event to the calendar using this function 'create_ics_event'.
+When using this function 'create_ics_event', you must pass in the appropriate format of duration {{"minutes": 60}} or {{"hours": 1}} for example.
+Upon successfully updating the calendar, please inform the user politely that the event has been scheduled, along with the details.
+
+Always present available times in a clear, neat, and readable format.
+It is critical that you only provide or suggest dates and times that are confirmed as available by the calendar data. Do not make up or infer availability that is not explicitly shown.
+
+Do not mention the specific name of the calendar tool or function you are using to the end user.
+
+The current date and time is {format_now}.
+"""
 
 user_proxy_agent = UserProxyAgent(
     name="User",
@@ -37,18 +58,14 @@ assistant_agent = AssistantAgent(
     name="Ollama Assistant",
     llm_config=ollama_config,  
     system_message=message,
-    function_map={"get_calendar": get_calendar},  
+    function_map={"get_calendar": get_calendar, "create_ics_event": create_ics_event},  
 )
 
-llm_config = {
-    "model": "mistral",
-    "api_type": "ollama",
-    "temperature": 0.5,
-}
+
 trip_response_agent = ConversableAgent(
     name="travel agent",
     system_message="""
-    when you get a message print now i am planing a trip.
+    when you get a message print now i am planning a trip.
     Given the travel details, predict the time taken and transport mode in Melbourne, Australia. 
     Format the response as:
     {
@@ -62,6 +79,59 @@ trip_response_agent = ConversableAgent(
     llm_config=llm_config,
 )
 
+documentWriter = ConversableAgent(
+    name="Writer Agent",
+    system_message="""
+    You are a helpful agent that is an expert at generating HTML content for travel reports.
+
+    When you receive a message containing travel details in a dictionary format like:
+    {
+        "start": {"name": "start_location"},
+        "destination": {"name": "destination_location"},
+        "time_taken": "predicted_time_taken",
+        "transport": "predicted_transport_mode"
+        "date & day": "date and day you have planned for"
+    }
+    AND a list of available dates.
+
+    Your task is to: 
+    1. Generate the full HTML content for the report based on this information, using the provided template structure. 
+    2. You must call this function 'document_creator' and pass the HTML as the string parameter.
+    3. Output what is being passed into the function 'document_creator' as the HTML string, and EXECUTE the function.
+    4. After you call the 'document_creator' tool, kindly let the user know of any errors that might occur during the function call.
+    
+    Template Structure:
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="UTF-8">
+    <title>Agent Report</title>
+    <style>
+      body { font-family: serif; }
+      h1 { color: green; }
+      p { font-size: 12pt; margin-bottom: 5px;}
+      h2, h3 { color: navy; margin-top: 20px; margin-bottom: 10px;}
+      ul, ol { margin-bottom: 10px;}
+      strong { font-weight: bold; }
+    </style>
+    </head>
+    <body>
+        <h2>[Fill Title Here]</h2>
+        <ol>
+            [Create list items <li> for available dates here]
+        </ol>
+        <h3>Travel Details:</h3>
+        <p><strong>Start Location:</strong> [Fill Start Location Here]</p>
+        <p><strong>Destination: </strong> [Fill Destination Here]</p>
+        <p><strong>Time Taken: </strong> [Fill Time Taken Here]</p>
+        <p><strong>Transport Mode: </strong> [Fill Transport Mode Here]</p>
+        <p><strong>Date&Time: </strong> [Fill Date & Time Here]</p>
+    </body>
+    </html>
+    """,
+    llm_config=ollama_config,
+    function_map={"document_creator": document_creator},  
+)
 
 register_function(
     get_calendar,
@@ -70,8 +140,23 @@ register_function(
     description="Get the availabilities as a list",
 )
 
+register_function(
+    create_ics_event,
+    caller=assistant_agent,  
+    executor=user_proxy_agent,  
+    description="Create and save a calendar event to a .ics file given name, start time, duration (as a dict), and optional location and description",
+)
+
+register_function(
+    document_creator,
+    caller=documentWriter,  
+    executor=user_proxy_agent,  
+    description="Creates a pdf document from a HTML string",
+)
+
+
 groupchat = autogen.GroupChat(
-    agents=[user_proxy_agent, assistant_agent, trip_response_agent],
+    agents=[user_proxy_agent, assistant_agent, trip_response_agent, documentWriter],
     messages=[],
     max_round=10
 )
@@ -86,16 +171,25 @@ chat_results = user_proxy_agent.initiate_chats(
     [
         {
             "recipient": assistant_agent,
-            "message": "Please check my availability using the 'get_calendar' function and suggest times where a trip can be planned",
-            "max_turns": 1,
-            "summary_method": "last_msg",
+            #"message": "Please check my availability using the 'get_calendar' function and suggest times where a trip can be planned",
+            "message": "Schedule a 45-minute meeting with Jane Doe on Thursday at 3:30 PM to discuss the Q2 marketing strategy. Add it to my work calendar.",
+            "max_turns": 2,
+            "summary_method": "reflection_with_llm",
         },
         {
             "recipient": trip_response_agent,
             "message": "I want to get to Richmond station by 10:30am from Pakenham Station create a trip based on my availability",
             "max_turns": 1, # One revision
             "summary_method": "last_msg",
-        },   
+        },
+        {
+            "recipient": documentWriter,
+            #"message": "Please create a document for me using the 'document_creator' function.",
+            "message": '''{"start": {"name": "Melbourne"}, "destination": {"name": "Sydney"}, "time_taken": "1 hour", "transport": "flight", "date & day": "2025-04-25"}
+                Available Dates: ["2025-04-25", "2025-04-26"]''',
+            "max_turns": 3, 
+            "summary_method": "reflection_with_llm",
+        },      
     ]
 )
 
--- tools.py
+++ tools.py
@@ -1,8 +1,10 @@
-import icalendar
-from ics import Calendar
+from ics import Calendar, Event
 from datetime import datetime, timedelta
 from pathlib import Path
-import pytz
+from xhtml2pdf import pisa
+import io
+import os
+import dateparser
 
 def get_calendar():
     """Open and read the calendar file"""
@@ -16,3 +18,68 @@ def get_calendar():
     formatted_events = ("\n".join(events))
     print(formatted_events)
     return formatted_events
+
+def create_ics_event(name: str, start_time: str, duration: dict, location: str = "", description: str = ""):
+    """Create a python dict based on the extracted event details"""
+    event_data = {
+        "name": name,  # title
+        "description": description, # optional
+        "location": location,
+        "start_time": dateparser.parse(start_time),
+        "duration": duration # default duration is 60 minutes if none specified 
+    }
+    print("Python Dict Created")
+    """Open and write an event into the calendar file using the extracted event data"""
+    ics_path = Path("random_calendar.ics")
+
+    with open(ics_path, 'r') as f:
+         calendar = Calendar(f.read()) # parse existing events
+    print("Successfully opened calendar file for reading")
+    event = Event()
+
+    # new event data
+    event.name = event_data["name"]
+    event.begin = event_data["start_time"]
+    event.duration = timedelta(**event_data["duration"])
+    event.location = event_data["location"]
+    event.description = event_data["description"]
+    print("Converted event details from python dict extracted details")
+    # add new event
+    calendar.events.add(event)
+    print("Added event details")
+    with open(ics_path, 'w') as f:
+        f.writelines(calendar.serialize_iter()) # writes calendar event into ics file
+    
+    print(f"📅 Creating: '{name}' at {start_time} for {duration} mins at {location}")
+
+def document_creator(content: str, base_filename: str ="report") -> str:
+    print("📄 DOCUMENT_CREATOR TOOL CALLED")
+    print(content[:100])  # Preview
+    main_content = content
+    print("being called")
+    script_path = os.path.abspath(__file__)
+
+    script_directory = os.path.dirname(script_path)
+    print("script directory")
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    output_filename = f"{base_filename}_{timestamp}.pdf"
+    print("filename")
+    output_filepath = os.path.join(script_directory, output_filename)
+    print("filepath")
+
+    source_html = io.BytesIO(content.encode("utf-8"))
+    print("sourcehtml")
+    with open(output_filepath, "w+b") as output_pdf:
+            pisa_status = pisa.CreatePDF(
+                source_html,
+                dest=output_pdf
+            )
+    print ("open pdf")        
+
+    if not pisa_status.err:
+        return 'Document Created'
+    else:
+        return 'Document not created'
+
+#create_ics_event("Q2 Marketing strategy with Jane Doe", "April 24, 2025 at 3:30 PM", {'minutes': 45}, "", "")
