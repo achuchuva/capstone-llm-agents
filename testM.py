560e6331c725149d9ba903cead777d9dd4c37cc5
Joel Downie <104540652@student.swin.edu.au>
2025-05-13 18:24:20 +1000

    .

--- testM.py
+++ testM.py
@@ -1,116 +1,52 @@
-import autogen
-import os
-#https://github.com/Andyinater/AutoGen_MemoryManager
-# Where the memory path will be located.
-mem_path = "MemFile.txt"
+from sentence_transformers import SentenceTransformer
+import numpy as np
+import chromadb as DB
 
 
-llm_config = {
-    "model": "llama3.2",
-    "api_type": "ollama",
-    "temperature": 0.5,
-}
+class VectorDatabase:
+   def __init__(self, agent: str, embedding_model_name: str = 'all-MiniLM-L6-v2'):
+       self.model = SentenceTransformer(embedding_model_name)
+       self.client = DB.PersistentClient(path=agent)
+       self.agent = agent
+       self.memory = self.client.get_or_create_collection(name=self.agent)
 
 
-def rewrite_memory(memory):
-    if not os.path.exists(mem_path):
-        with open(mem_path, 'w') as f:
-            f.write(memory + '|')
-
-    else:
-        with open(mem_path, 'w') as f:
-            f.write(memory + '|')
-
-    return True
-
-def read_from_memory():
-    if os.path.exists(mem_path):
-        with open(mem_path, 'r') as f:
-            d = f.read()
-    else:
-        d = "NO LONG TERM MEMORY FOUND"
-    return d
-
-def write_to_memory_intent(message):
-    user_proxy_for_functions.initiate_chat(
-    mem_manager,
-    message=message,
-)
-
-def read_from_memory_intent():
-    if os.path.exists(mem_path):
-        with open(mem_path, 'r') as f:
-            d = f.read()
-    else:
-        d = "NO LONG TERM MEMORY FOUND"
-    return d
+   def get_id(self):
+       return len(self.memory.get()['ids']) + 1
 
 
-# create the MemoryManagerAgent
-mem_manager = autogen.AssistantAgent(
-    name="mem_manager",
-    llm_config=llm_config,
-    system_message = """You are a helpful AI assistant acting as a memory manager. You will be asked to either write a message to memory, or to read the memory.
-    If you are being asked to write to memory, you should:
-        1. Read the existing memory: Before you decide if a memory should be added, you need to know what memories already exist.
-        2. If you deem the message to be new information that is not represented in the existing memories, you should rewrite the existing memories such that the information is now incorporated.
+   def add(self, text: str, metadata: dict = None):
 
-    The memory is formatted as a series of statements seperated by the '|' character.
-
-    For example, the existing memory might look like this:
-        "User is allergic to seafood| User likes dogs| User is from Canada"
-
-    In this example, if you were requested to write a message to memory that was "User likes cats", when you go to rewrite the memory, it should look like this:
-        "User is allergic to seafood| User likes dogs, cats| User is from Canada"
+       embedding = self.model.encode(text)
+       memory = self.client.get_collection(name=self.agent)
+       memory.add(
+           embeddings=[embedding],
+           documents=[text],
+           ids=[str(self.get_id())],
+           metadatas=[metadata]
+       )
 
 
-    Sometimes it might be best to remove content from the memory. For example, if the existing memory looked like this:
-        "User is allergic to seafood| User likes dogs, cats| User is from Canada"
-
-    And you were asked to write a message to memory that was "User does not like dogs", when you go to rewrite the memory, it should look like this:
-        "User is allergic to seafood| User likes cats| User is from Canada"
-
-    The changes to the rewritten memory can span multiple statements, if appropriate. The point is to keep the entire memory as accurate and representative as possible.
-
-    Do not participate in any form of conversation.
-
-    After you have finished your task, respond with "TERMINATE"
-""",
-)
+   def search(self, query: str, top_k: int = 1):
+       query_embedding = self.model.encode(query)
+       results = self.memory.query(
+           query_embeddings=[query_embedding],
+           n_results=top_k,
+           include=['documents', 'metadatas', 'distances']
+       )
+       return results
 
 
+if __name__ == "__main__":
+   memory_db = VectorDatabase(agent='AssistantMemory')
 
-
-
-
-
-# create the ConversingAgent
-conversing_agent = autogen.AssistantAgent(
-    name="assistant",
-    llm_config=llm_config,
-    system_message = "You are a helpful AI assistant, with even more helpful tools. Use one or more tools BEFORE talking to the user. Converse with the user to help them achieve their goals, but use tools too. write things to memory often. read from memory before asking the user. It is critically important that, whenever the user gives details about themselves or their preferences, you write it to memory. If this is the start of a new conversation, the very first thing you must do is read your memory. If no long term memory is found, you should start by asking who you're talking to.",
-)
-
-# create the UserProxyAgent used by the human to converse
-user_proxy = autogen.UserProxyAgent(
-    name="user_proxy",
-    human_input_mode="ALWAYS",
-    max_consecutive_auto_reply=10,
-    code_execution_config={"work_dir": "_test", "use_docker": False},#maybe a docker shoudld be needed?
-    function_map={"write_to_memory_intent": write_to_memory_intent, "read_from_memory_intent": read_from_memory_intent, "rewrite_memory": rewrite_memory, "read_from_memory": read_from_memory},
-)
-
-# create the UserProxyAgent_ForFunctions to be used by the MemoryManager to execute function calls in chat
-user_proxy_for_functions = autogen.UserProxyAgent(
-    name="user_proxy_for_functions",
-    human_input_mode="TERMINATE",
-    max_consecutive_auto_reply=10,
-    code_execution_config={"work_dir": "_test", "use_docker": False},
-    function_map={"write_to_memory_intent": write_to_memory_intent, "read_from_memory_intent": read_from_memory_intent, "rewrite_memory": rewrite_memory, "read_from_memory": read_from_memory},
-)
-
-# the assistant receives a message from the user, which contains the task description
-user_proxy.initiate_chat(
-    conversing_agent,
-    message="""(User is present)""",
-)
+   memory_db.add("The sky is blue.", {"timestamp": "10:00PM", "source": "observation"})
+   memory_db.add("The Ground is green.", {"timestamp": "11:00PM", "source": "observation"})
+   print("Results1 bellow:")
+   print(memory_db.search('blue'))
+   results = memory_db.search('blue')
+   first_doc = results['documents'][0][0]#https://www.w3schools.com/python/numpy/numpy_array_indexing.asp
+   print("Results1 content bellow:")
+   print(first_doc)
+   print("Results2 bellow:")
+   print(memory_db.search('green'))
