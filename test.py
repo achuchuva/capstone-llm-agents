bd85a7e915fca87157d37bc61a56bee952cd888e
Joel Downie <104540652@student.swin.edu.au>
2025-05-27 15:23:07 +1000

    Update test.py

--- test.py
+++ test.py
@@ -418,10 +418,8 @@ import sys
 
 from PyQt6.QtWidgets import (
     QApplication,
-    QComboBox,
-    QFormLayout,
-    QLineEdit,
-    QStackedLayout,
+    QCheckBox,
+    QTabWidget,
     QVBoxLayout,
     QWidget,
 )
@@ -429,36 +427,34 @@ from PyQt6.QtWidgets import (
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("QStackedLayout Example")
+        self.setWindowTitle("QTabWidget Example")
+        self.resize(270, 110)
         # Create a top-level layout
         layout = QVBoxLayout()
         self.setLayout(layout)
-        # Create and connect the combo box to switch between pages
-        self.pageCombo = QComboBox()
-        self.pageCombo.addItems(["Page 1", "Page 2"])
-        self.pageCombo.activated.connect(self.switchPage)
-        # Create the stacked layout
-        self.stackedLayout = QStackedLayout()
-        # Create the first page
-        self.page1 = QWidget()
-        self.page1Layout = QFormLayout()
-        self.page1Layout.addRow("Name:", QLineEdit())
-        self.page1Layout.addRow("Address:", QLineEdit())
-        self.page1.setLayout(self.page1Layout)
-        self.stackedLayout.addWidget(self.page1)
-        # Create the second page
-        self.page2 = QWidget()
-        self.page2Layout = QFormLayout()
-        self.page2Layout.addRow("Job:", QLineEdit())
-        self.page2Layout.addRow("Department:", QLineEdit())
-        self.page2.setLayout(self.page2Layout)
-        self.stackedLayout.addWidget(self.page2)
-        # Add the combo box and the stacked layout to the top-level layout
-        layout.addWidget(self.pageCombo)
-        layout.addLayout(self.stackedLayout)
+        # Create the tab widget with two tabs
+        tabs = QTabWidget()
+        tabs.addTab(self.generalTabUI(), "General")
+        tabs.addTab(self.networkTabUI(), "Network")
+        layout.addWidget(tabs)
 
-    def switchPage(self):
-        self.stackedLayout.setCurrentIndex(self.pageCombo.currentIndex())
+    def generalTabUI(self):
+        """Create the General page UI."""
+        generalTab = QWidget()
+        layout = QVBoxLayout()
+        layout.addWidget(QCheckBox("General Option 1"))
+        layout.addWidget(QCheckBox("General Option 2"))
+        generalTab.setLayout(layout)
+        return generalTab
+
+    def networkTabUI(self):
+        """Create the Network page UI."""
+        networkTab = QWidget()
+        layout = QVBoxLayout()
+        layout.addWidget(QCheckBox("Network Option 1"))
+        layout.addWidget(QCheckBox("Network Option 2"))
+        networkTab.setLayout(layout)
+        return networkTab
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
